0756001d911a968fa0cc0535639f71122fa43f1e
JosephZeller <joseph.m.zeller@gmail.com>
2019-12-24 11:00:50 -0600

    Create Skater.java

---
+++ Skater.java
@@ -0,0 +1,39 @@
+public class Skater{
+  public String name;
+  public String id;
+  public int age;
+  public String team;
+  public String position;
+  public int gamesPlayed;
+  public int goals;
+  public int assists;
+  public int points;
+  public int plusMinus;
+  public int penaltiesInMinutes;
+  public double pointShares;
+  public int evenStrengthGoals;
+  public int powerPlayGoals;
+  public int shortHandedGoals;
+  public int gameWinningGoals;
+  public int evenStrenghtAssists;
+  public int powerPlayAssists;
+  public int shortHandedAssists;
+  public int shotsOnGoal;
+  public double shootingPercentage;
+  public int totalShotAttempts;
+  public double timeOnIce;
+  public double averageTimeOnIce;
+  public int faceOffWins;
+  public int faceOffLosses;
+  public double faceOffPercentage;
+  public int blocks;
+  public int hits;
+  public int takeaways;
+  public int giveaways;
+
+  public Skater(String name, String id){
+    this.name = name;
+    this.id = id;
+  }
+
+}
